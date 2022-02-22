0780f8d27c45886868b847b0a555e390ff50bc93
mariorzgz <mario.rod.gonz97@gmail.com>
2022-02-22 11:40:14 +0100

    Created tour model; Added associations between tour and user; Added validations for tour & model

--- app/models/tour.rb
+++ app/models/tour.rb
@@ -1,3 +1,10 @@
 class Tour < ApplicationRecord
   belongs_to :user
+
+  validates :name, presence: true
+  validates :city, presence: true
+  validates :starting_point, presence: true
+  validates :description, presence: true, length: { minimum: 100 }
+  validates :duration, presence: true
+  validates :price, presence: true
 end
