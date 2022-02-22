d5cee704baa0aa89c8ddcb059b65499e9f746449
mariorzgz <mario.rod.gonz97@gmail.com>
2022-02-22 12:07:45 +0100

    Added maximum length for the description of 750 chars

--- app/models/tour.rb
+++ app/models/tour.rb
@@ -4,7 +4,7 @@ class Tour < ApplicationRecord
   validates :name, presence: true
   validates :city, presence: true
   validates :starting_point, presence: true
-  validates :description, presence: true, length: { minimum: 100 }
+  validates :description, presence: true, length: { in: 100..750 }
   validates :duration, presence: true
   validates :price, presence: true
 end
