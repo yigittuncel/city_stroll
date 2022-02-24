71769a56f2be36b9fb60a42b6e5bd4ea8a6c3742
Christina Borensky <christina.borensky@web.de>
2022-02-24 15:12:22 +0100

    Changed photos & description

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,17 +21,24 @@ users = []
 end
 
 tour_photos = [
-  "https://upload.wikimedia.org/wikipedia/commons/7/73/Rathaus_and_Marienplatz_from_Peterskirche_-_August_2006.jpg",
-  "https://www.nationsonline.org/gallery/Germany/Theatiner-Church-Munich.jpg",
-  "https://upload.wikimedia.org/wikipedia/commons/d/da/Frauenkirche_Munich_-_View_from_Peterskirche_Tower2.jpg",
-  "https://3iqhm91wtiv21y4zza4dqwj2-wpengine.netdna-ssl.com/wp-content/uploads/Germany-Munich-Upscale-Shopping-District.jpg",
-  "https://upload.wikimedia.org/wikipedia/commons/0/06/Basilika_Sankt_Paul_Munich_by_night.jpg",
-  "https://www.gpsmycity.com/img/gd_sight/16189.jpg"
+  "https://cdn.pixabay.com/photo/2018/04/25/09/26/eiffel-tower-3349075_1280.jpg",
+  "https://images.unsplash.com/photo-1587789202069-f57c846b85db?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80",
+  "https://images.unsplash.com/photo-1503152394-c571994fd383?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+  "https://images.unsplash.com/photo-1586172992865-4dfff50bd23a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+  "https://cdn.pixabay.com/photo/2013/04/11/19/46/building-102840_1280.jpg",
+  "https://images.unsplash.com/photo-1630219694734-fe47ab76b15e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=752&q=80"
 ]
 
-tour_names = ["Old town", "Viktualienmarkt", "Hop-On Hop-Off", "City Tour", "Spooky Tour of Old Town", "Romantic Road"]
-starting_points = ["Marienplatz", "Rathaus", "Englischer Garten", "Nymphenburg Palace", "Munich Residenz", "Olympia Park"]
-ending_points = ["Viktualienmarkt", "BMW Museum", "Peterskirche", "Deutsches Museum", "Asamkirche"]
+tour_names = ["Eiffel Tower", "Sagrada Familia", "Acropolis", "Walking Tour", "Louvre Museum", "Park Güell"]
+starting_points = ["Champs de Mars", "Sagrada Familia", "Apostolou Pavlou 11, Athens 118 51", "Anne Frank house", "Arc du Carrousel", "Carretera del Carmel"]
+ending_points = ["Summit of Eiffel Tower", "Sagrada Familia", "Acropolis museum", "Anne Frank house", "Louvre museum", "Park Güell"]
+city = ["Paris", "Barcelona", "Athens", "Amsterdam", "Paris", "Barcelona"]
+description = ["Make the best use of your precious time in Paris with this direct access ticket to the Eiffel Tower. Choose from a 2nd level + summit or 2nd level-only ticket and follow a certified guide to learn the fascinating history of this world-famous landmark.",
+               "Make your visit to the Sagrada Familia unforgettable with a 1.5-hour guided tour of Gaudí’s masterpiece. See the Basilica’s dream-like façade and interior in a small-group or private tour setting accompanied by an expert guide.",
+               "Enjoy skip-the-ticket line access and discover the cradle of Western civilization on a guided visit to the Acropolis. Choose from either a small-group or private guided experience and indulge in tasty Greek treats when you upgrade to include a street food tour in Athen's old town.",
+               "Visit famous sites of Amsterdam downtown and get an overview of the city on this 2.5-hour walking tour dedicated to culture and history. Profit from tips every adventurous traveler needs to know and make the best of your trip. Listen to your guide telling you which architectural designs were popular in the past and get an idea of the people of Amsterdam.",
+               "Get priority entry access to the Louvre Museum and be led by an expert guide on a carefully planned route designed to maximize your time and give you a greater insight into the museum’s top highlights.",
+               "Meet your guide in front of the Park Güell, skip the ticket line and discover this magnificent park designed by Antoni Gaudí more than 100 years ago. See the magnificent bridges, viaduct, and monumental zone."]
 
 tours = Array.new(6)
 
