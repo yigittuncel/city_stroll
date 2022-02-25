4e37d7ef388d64c3fa809a91d1892cf5d69ee8c8
yigit tuncel <yigittuncel35@gmail.com>
2022-02-25 11:48:56 +0100

    tour form updated

--- app/models/tour.rb
+++ app/models/tour.rb
@@ -9,9 +9,11 @@ class Tour < ApplicationRecord
   validates :name, presence: true
   validates :city, presence: true
   validates :starting_point, presence: true
+  validates :ending_point, presence: true
   validates :description, presence: true, length: { in: 100..750 }
   validates :duration, presence: true
   validates :price, presence: true
+  validates :photo, presence: true
 
   def minutes_to_hours
     hours = duration / 60
